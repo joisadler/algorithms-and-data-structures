a9d7db512a031f0c4d268b7a34bddd2706230426
Yossi Adler <joisadler@gmail.com>
2017-06-15 19:47:24 +0300

    Add a bubble-sort.js

---
+++ bubble-sort.js
@@ -0,0 +1,24 @@
+const bubbleSort = (array) => {
+  const result = array.slice();
+  let swap = false;
+
+  for (let i = 1; i < result.length - 2; i += 1) {
+    for (let j = 0; j <= result.length - i; j += 1) {
+      if (result[j] > result[j + 1]) {
+        const temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+        swap = true;
+      }
+    }
+    if (swap === false) {
+      return result;
+    }
+  }
+  return result;
+};
+
+console.log(bubbleSort([5, 1, 4, 2, 8])); // [ 1, 2, 4, 5, 8 ]
+console.log(bubbleSort([45, 0, 4.5, 77, 2, 0, 838, 7])); // [ 0, 0, 2, 4.5, 7, 45, 77, 838 ]
+console.log(bubbleSort([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ]
+console.log(bubbleSort(['a', 'b', 'd', 'c', 'f', 'g', 'e', 'z'])); // [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'z' ]
