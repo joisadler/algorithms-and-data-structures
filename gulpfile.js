e813553e2cc97d056b75a7f8e38824a9ddf41818
Yossi Adler <joisadler@gmail.com>
2017-07-08 02:23:38 +0300

    change gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,27 @@
-"use strict";
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const watch = require('gulp-watch');
+const plumber = require('gulp-plumber');
 
-var gulp = require('gulp'),
-    babel = require('gulp-babel'),
-    watch = require('gulp-watch'),
-    plumber = require('gulp-plumber'),
-
-path = {
-    src: {
-        //js: 'src/*.js'
-        js: ['src/algorithms/sort/*.js', 'src/data-structures/*.js'],
-    },
-    dist: {
-        js: 'compiled'
-        //js: 'dist/'
-    },
+const path = {
+  src: {
+    js: ['src/algorithms/sort/*.js', 'src/data-structures/*.js'],
+  },
+  dist: {
+    js: 'compiled',
+  },
 };
 
-gulp.task('6to5', function () {
-    gulp.src(path.src.js)
-        .pipe(plumber())
-        .pipe(babel())
-        .pipe(plumber.stop())
-        .pipe(gulp.dest(path.dist.js));
+gulp.task('6to5', () => {
+  gulp.src(path.src.js)
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(plumber.stop())
+    .pipe(gulp.dest(path.dist.js));
 });
 
-gulp.task('watch', ['6to5'], function () {
-    gulp.watch([path.src.js], [babel]);
+gulp.task('watch', ['6to5'], () => {
+  gulp.watch([path.src.js], [babel]);
 });
 
 gulp.task('default', ['watch']);
