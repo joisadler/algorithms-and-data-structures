1e78ecad1cb6ec0317372afdb12f4d020e9e44f3
Yossi Adler <joisadler@gmail.com>
2017-07-10 10:16:43 +0300

    add quicksort test

---
+++ __tests__/quicksort.test.js
@@ -0,0 +1,8 @@
+const quickSort = require('../src/algorithms/sort/quicksort');
+
+test('bubble-sort', () => {
+  expect(quickSort([5, 1, 4, 2, 8])).toEqual([1, 2, 4, 5, 8]);
+  expect(quickSort([45, 0, 4.5, 77, 2, 0, 838, 7])).toEqual([0, 0, 2, 4.5, 7, 45, 77, 838]);
+  expect(quickSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+  expect(quickSort(['a', 'b', 'd', 'c', 'f', 'g', 'e', 'z'])).toEqual(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'z']);
+});
--- src/algorithms/sort/quicksort.js
+++ src/algorithms/sort/quicksort.js
@@ -1,15 +1,18 @@
-const quicksort = (arr) => {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  const pivot = arr[0];
-  const rest = arr.splice(0, 1);
-  const less = arr.slice().filter(item => item < pivot, rest);
-  const greaterOrEqual = arr.slice().filter(item => item >= pivot, rest);
-  return [].concat(quicksort(less), pivot, quicksort(greaterOrEqual));
+module.exports = (arr) => {
+  const sort = (list) => {
+    if (list.length <= 1) {
+      return list;
+    }
+    const pivot = list[0];
+    const rest = list.splice(0, 1);
+    const less = list.slice().filter(item => item < pivot, rest);
+    const greaterOrEqual = list.slice().filter(item => item >= pivot, rest);
+    return [].concat(sort(less), pivot, sort(greaterOrEqual));
+  };
+  return sort(arr);
 };
 
-console.log(quicksort([5, 1, 4, 2, 8])); // [ 1, 2, 4, 5, 8 ]
-console.log(quicksort([45, 0, 4.5, 77, 0, 2, 838, 7, 0])); // [0, 0, 0, 2, 4.5, 7, 45, 77, 838 ]
-console.log(quicksort([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ]
-console.log(quicksort(['a', 'b', 'd', 'c', 'f', 'g', 'e', 'z'])); // [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'z' ]
+//console.log(quicksort([5, 1, 4, 2, 8])); // [ 1, 2, 4, 5, 8 ]
+//console.log(quicksort([45, 0, 4.5, 77, 0, 2, 838, 7, 0])); // [0, 0, 0, 2, 4.5, 7, 45, 77, 838 ]
+//console.log(quicksort([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ]
+//console.log(quicksort(['a', 'b', 'd', 'c', 'f', 'g', 'e', 'z'])); // [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'z' ]
