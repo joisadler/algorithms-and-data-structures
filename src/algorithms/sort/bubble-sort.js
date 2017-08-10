1707677a10af2bc539b1e83ce2d00a4d068e0524
Yossi Adler <joisadler@gmail.com>
2017-08-10 08:44:56 +0300

    testing destructuring assignment in bubble-sort

--- src/algorithms/sort/bubble-sort.js
+++ src/algorithms/sort/bubble-sort.js
@@ -5,9 +5,7 @@ export default (array) => {
   for (let i = 0; i < result.length - 1; i += 1) {
     for (let j = 0; j <= result.length - i; j += 1) {
       if (result[j] > result[j + 1]) {
-        const temp = result[j];
-        result[j] = result[j + 1];
-        result[j + 1] = temp;
+        [result[j], result[j + 1]] = [result[j + 1], result[j]];
         swap = true;
       }
     }
